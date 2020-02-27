23cd18cfe58d03ef6f37e428f94eb57ebe61b94f
DimaAntoniuk <dimaantoniuk09@gmail.com>
2020-02-27 17:15:37 +0200

    feat: onPressC(), onPressD() BREACKING_CHANGE: done fix: clean code

--- Clock/src/main.cpp
+++ Clock/src/main.cpp
@@ -13,7 +13,7 @@
 #define COLUMNS 4
 #define BUZZER A12
 #define BEEP_SHORT_DELAY 50
-#define BEEP_LONG_DELAY 150
+#define BEEP_LONG_DELAY 250
 
 LiquidCrystal lcd(RS, ENABLE, D4, D5, D6, D7);
 
@@ -21,18 +21,20 @@ char hexaKeys[ROWS][COLUMNS] = {
     {'1', '2', '3', 'A'},
     {'4', '5', '6', 'B'},
     {'7', '8', '9', 'C'},
-    {'#', '0', '*', 'D'}};
+    {'*', '0', '#', 'D'}};
 byte rowPins[ROWS] = {45, 44, 43, 42};
 byte columnPins[COLUMNS] = {46, 47, 48, 49};
 
 Keypad keypad = Keypad(makeKeymap(hexaKeys), rowPins, columnPins, ROWS, COLUMNS);
+
+bool isAlarm = false, isDisabled = false;
 
 struct Time
 {
   uint8_t second, minute, hour;
 };
 Time time = {0, 0, 0};
-Time alarm;
+Time alarm = {0, 0, 0};
 
 ISR(TIMER1_COMPA_vect)
 {
@@ -46,6 +48,10 @@ ISR(TIMER1_COMPA_vect)
         time.hour = 0;
     }
   }
+
+  if (!isDisabled && time.hour == alarm.hour && time.minute == alarm.minute && time.second == alarm.second) {
+    isAlarm = true;
+  }
 }
 
 inline void showTime(const Time &time);
@@ -58,6 +64,7 @@ inline bool checkTimeFormat(const Time &time);
 inline void shortBeep();
 inline void doubleShortBeep();
 inline void longBeep();
+inline void playAlarm();
 
 void setup()
 {
@@ -77,7 +84,7 @@ void setup()
 void loop()
 {
   char selectedKey = keypad.getKey();
-  delay(50);
+  delay(100);
   switch (selectedKey)
   {
   case 'A':
@@ -105,6 +112,10 @@ void loop()
     delay(100);
     break;
   }
+
+  if (isAlarm) {
+    playAlarm();
+  }
 }
 
 inline void onPressA()
@@ -118,18 +129,20 @@ inline void onPressA()
   char enteredKey;
   while (currentSize < 6) {
     if (enteredKey = keypad.getKey()) {
+      shortBeep();
       if (int(enteredKey) <= int('9') && int(enteredKey) >= int('0')) {
         lcd.setCursor(currentSize + currentSize / 2, 1);
         lcd.print(enteredKey);
         numbers[currentSize] = enteredKey - '0';
         currentSize ++;
         if (!(currentSize % 2) && currentSize < 5 && currentSize > 0) {
-        lcd.print(":");
-      }
+          lcd.print(":");
+        }
       } else {
         lcd.setCursor(11, 0);
         lcd.print("Error");
-        delay(500);
+        longBeep();
+        delay(300);
         lcd.setCursor(11, 0);
         lcd.print("     ");
       }
@@ -159,14 +172,101 @@ inline void onPressA()
 
 inline void onPressB()
 {
+  Time enteredTime;
+  int numbers[4];
+  int currentSize = 0;
+  lcd.clear();
+  lcd.setCursor(0, 0);
+  lcd.print("Alarm:");
+  char enteredKey;
+  while (currentSize < 4) {
+    if (enteredKey = keypad.getKey()) {
+      shortBeep();
+      if (int(enteredKey) <= int('9') && int(enteredKey) >= int('0')) {
+        lcd.setCursor(currentSize + currentSize / 2, 1);
+        lcd.print(enteredKey);
+        numbers[currentSize] = enteredKey - '0';
+        currentSize ++;
+        if (!(currentSize % 2) && currentSize < 3 && currentSize > 0) {
+          lcd.print(":");
+        }
+      } else {
+        lcd.setCursor(11, 0);
+        lcd.print("Error");
+        longBeep();
+        delay(300);
+        lcd.setCursor(11, 0);
+        lcd.print("     ");
+      }
+    }
+  }
+
+  enteredTime.hour = numbers[0] * 10 + numbers[1];
+  enteredTime.minute = numbers[2] * 10 + numbers[3];
+  enteredTime.second = 0;
+
+  if (checkTimeFormat(enteredTime)) {
+    if (apply()) {
+      alarm = enteredTime;
+    }
+  } else {
+    lcd.clear();
+    lcd.setCursor(0, 0);
+    lcd.print("Wrong time");
+    lcd.setCursor(0, 1);
+    lcd.print("Again?");
+    if (apply()) {
+      onPressB();
+    }
+  }
+  showTime(time);
 }
 
 inline void onPressC()
 {
+  showTime(alarm);
+  lcd.setCursor(0, 1);
+  lcd.print("Back-# ");
+  lcd.print(isDisabled ? " Enable-A":"Disable-D");
+
+  char currentKey;
+  while(true){
+    if(currentKey = keypad.getKey()) {
+      if (currentKey == '#') {
+        break;
+      } else if ((currentKey == 'D' && !isDisabled) || (currentKey == 'A' && isDisabled)) {
+        isDisabled = !isDisabled;
+        break;
+      }
+      delay(50);
+    }
+  }
+  doubleShortBeep();
+  lcd.clear();
 }
 
 inline void onPressD()
 {
+}
+
+inline void playAlarm() {
+  lcd.setCursor(0, 1);
+  lcd.print("Disable?");
+  lcd.setCursor(11, 1);
+  lcd.print("Yes-#");
+  
+  char currentKey;
+  uint8_t iteration = 0;
+  
+  while (iteration < 10) {
+    longBeep();
+    if (keypad.getKey() == '#') {
+      isAlarm = false;
+      break;
+    }
+    iteration++;
+    delay(50);
+  }
 }
 
 bool apply()
@@ -197,7 +297,6 @@ inline bool checkTimeFormat(const Time& time)
   return (time.hour < 24 && time.hour >= 0 
     && time.minute < 60 && time.minute >= 0
     && time.second < 60 && time.second >= 0);
-
 }
 
 inline void shortBeep()
@@ -228,10 +327,16 @@ inline void longBeep()
 inline void showTime(const Time &time)
 {
   lcd.clear();
-  lcd.setCursor(3, 0);
+  lcd.setCursor(0, 0);
+  if (time.hour < 10)
+    lcd.print("0");
   lcd.print(time.hour);
   lcd.print(":");
+  if (time.minute < 10)
+    lcd.print("0");
   lcd.print(time.minute);
   lcd.print(":");
+  if (time.second < 10)
+    lcd.print("0");
   lcd.print(time.second);
 }
